3f9429d97bbc244ca69ff241fc03adedb4ec9b44
asmape0104 <6086632642@g.ecc.u-tokyo.ac.jp>
2019-03-14 20:17:56 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+/**
+ * pukiwiki形式の文章をmd形式にする。
+ * @param {String} text 変換したい文章。
+ * @return {String} 変換した文章。
+ */
+module.exports = function(text) {
+    let result = text;
+
+    // 改行コード統一
+    result = result.replace(/\r\n/g, "\n");
+    result = result.replace(/\r/g, "\n");
+
+    // pukiプラグイン→md標準機能
+    // 水平線
+    result = result.replace(/^#hr/mg, '____');
+
+    // tatami
+    result = result.replace(/^#tatami\(([^,]*),([^\)]+)\)/mg, (...p) => {
+        return '*'.repeat(parseInt(p[2])) + (p[1] || ' ') + ' [close]';
+    })
+
+    // プラグインは&に統一する。見出しで#を使うので見出しより前でやる。
+    // result = result.replace(/^#([a-z])/mg, '&$1');
+
+    // リンク([[ページ名>リンク先]]型)
+    // [[ページ名]]でマッチしないように先にやる。
+    result = result.replace(/\[\[([^\]>]+)>([^\]]+)\]\]/g, '[$1]($2)');
+
+    // リンク([[ページ名]]型)
+    result = result.replace(/\[\[([^\]]+)\]\]/g, '<$1>');
+
+    // 階層箇条書き (-)
+    result = result.replace(/^(\-+)\s?([^\-])/mg, (...p) => {
+        let depth = p[1].length;
+        return ' '.repeat(depth - 1) + '- ' + p[2];
+    })
+
+    // 階層番号付き箇条書き (+)
+    result = result.replace(/^(\++)\s?([^\+])/mg, (...p) => {
+        let depth = p[1].length;
+        return ' '.repeat(depth - 1) + '1. ' + p[2];
+    })
+
+    // th
+    result = result.replace(/^\|(([^\|]+\|)+)h$/mg, (...p) => {
+        let colnum = p[1].split('|').length - 1;
+        return '|' + p[1] + "\n" + '|' + '----|'.repeat(colnum);
+    })
+
+    // 見出し
+    // 楽なので見出しのレベルが小さい方からやる
+    for (let i = 6; i >= 1; i--) {
+        let reg = new RegExp(`^\\*{${i}}\s?`, 'mg');
+        result = result.replace(reg, '#'.repeat(i) + ' ');
+    }
+
+    // 斜体
+    result = result.replace(/'''(.*?)'''/g, '*$1*');
+
+    // 太字
+    result = result.replace(/''(.*?)''/g, '**$1**');
+
+    // console.log(result);
+    return result;
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# puki2md
+[![npm version](https://badge.fury.io/js/puki2md.svg)](https://badge.fury.io/js/puki2md)
+
+## Overview
+Convert pukiwiki style code to markdown style
+
+## Usage
+```
+const puki2md = require('puki2md');
+puki2md('pukiwiki code here');
+```
